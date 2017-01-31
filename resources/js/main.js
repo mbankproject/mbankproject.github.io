c459ac4fcfbb0098a35feae40e695eb4c260d984
Jan Dzban <mbankproject@mail.ru>
2017-01-31 19:05:48 +0100

    Deposit & credit section, menu animations

--- resources/js/main.js
+++ resources/js/main.js
@@ -28,6 +28,22 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   })
 
+  Array.from($('.content .product > div, .content+.about > .close')).map(function(item, index, array) {
+    item.addEventListener('click', function(event) {
+      array.map(function(item2) {
+        item2.classList.remove('active');
+      })
+      item.classList.add('active');
+      Array.from($('.content+.about > section')).map(function(section) {
+        if(section.id === item.getAttribute('name')) {
+          section.classList.add('active');
+        } else {
+          section.classList.remove('active');
+        }
+      });
+    });
+  });
+
   document.addEventListener('scroll', function(event) {
     if(event.pageY+25 >= Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
       $('header#header nav')[0].classList.add('dark');
