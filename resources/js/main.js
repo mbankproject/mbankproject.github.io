32425b9389d8c5fec382a0e4cd8bc9ba3530ba33
Jan Dzban <mbankproject@mail.ru>
2017-01-31 16:07:30 +0100

    Deposits section, menu improvements

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,7 @@ function $(e) {
   return document.querySelectorAll(e);
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
   $('.burger')[0].addEventListener('click', function(event) {
     this.classList.toggle('active');
@@ -9,6 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
     $('header#header nav')[0].classList.toggle('active');
     $('.blackscreen')[0].classList.toggle('active');
   });
+
+  $('.blackscreen')[0].addEventListener('click', function(event) {
+    this.classList.toggle('active');
+    $('.dropDown')[0].classList.toggle('active');
+    $('header#header nav')[0].classList.toggle('active');
+    $('.burger')[0].classList.toggle('active');
+  });
+
+  Array.from($('.dropDown ul a')).map(function(item) {
+    item.addEventListener('click', function(event) {
+      window.location.hash = item.getAttribute('href');
+      $('.blackscreen')[0].classList.toggle('active');
+      $('.dropDown')[0].classList.toggle('active');
+      $('header#header nav')[0].classList.toggle('active');
+      $('.burger')[0].classList.toggle('active');
+    });
+  })
 
   document.addEventListener('scroll', function(event) {
     if(event.pageY+25 >= Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
