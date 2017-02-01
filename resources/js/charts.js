7094cc15d14261afce6a424aa6bd72945a0b2b3f
Jan Dzban <mbankproject@mail.ru>
2017-02-01 17:32:20 +0100

    Deposits and loans sections, highcharts, tables

---
+++ resources/js/charts.js
@@ -0,0 +1,93 @@
+
+window.addEventListener('load', function(event) {
+  var deposit_charts = new Array(4).fill(null).map(function(item, index) {
+    item = Highcharts.chart('deposit_chart'+(index+1), {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Oprocentowanie'
+      },
+      xAxis: {
+        title: {
+          text: 'Miesiąc'
+        }, labels: {
+          formatter: function() {
+            return this.value + '-miesiac'
+          }
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Oprocentowanie'
+        }, labels: {
+          formatter: function() {
+            return this.value + ' %'
+          }
+        }
+      },
+      series:[{
+        name: 'jan',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt(Math.sin(index/1.25)*100)/100;
+        }),
+      }, {
+        name: 'dzban',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt(Math.cos(index/1.25)*100)/100;
+        }),
+      }, {
+        name: 'huligan',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt((Math.random()*2-1)*100)/100;
+        }),
+      }]
+    });
+  });
+
+
+  var loan_charts = new Array(4).fill(null).map(function(item, index) {
+    item = Highcharts.chart('loan_chart'+(index+1), {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Oprocentowanie'
+      },
+      xAxis: {
+        title: {
+          text: 'Miesiąc'
+        }, labels: {
+          formatter: function() {
+            return this.value + '-miesiac'
+          }
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Oprocentowanie'
+        }, labels: {
+          formatter: function() {
+            return this.value + ' %'
+          }
+        }
+      },
+      series:[{
+        name: 'jan',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt((Math.random()*Math.sin(index+1)*2-1)*100)/100;
+        }),
+      }, {
+        name: 'dzban',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt((Math.random()*Math.sin(index+1)*2-1)*100)/100;
+        }),
+      }, {
+        name: 'huligan',
+        data: new Array(20).fill(0).map(function(item, index) {
+          return parseInt((Math.random()*Math.sin(index+1)*2-1)*100)/100;
+        }),
+      }]
+    });
+  });
+});
