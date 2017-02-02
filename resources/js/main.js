908d2f05d009677def03b31456e351c6b6d1a707
Jan Dzban <mbankproject@mail.ru>
2017-02-02 17:41:51 +0100

    JS rebuild & favicons

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,75 +1,94 @@
 'use strict';
 
-function $(e) {
-  return document.querySelectorAll(e);
+function $(e, b = document) {
+  return b.querySelectorAll(e);
 }
 
-function findPos(obj) {
+HTMLDocument.prototype.findPos = function(obj) {
     var curtop = 0;
-    if (obj.offsetParent) {
+    if(obj.offsetParent) {
         do {
             curtop += obj.offsetTop;
-        } while (obj = obj.offsetParent);
+        } while(obj = obj.offsetParent);
     return [curtop];
     }
 }
 
+HTMLDocument.prototype.toggleClass = function(className, elements) {
+  for(var element of elements) {
+    element.classList.toggle(className);
+  }
+}
+
+HTMLDocument.prototype.getScrollOffset = function() {
+  return window.pageYOffset || document.documentElement.scrollTop || 0;
+}
+
+HTMLDocument.prototype.getViewportHeight = function() {
+  return document.documentElement.clientHeight || window.innerHeight || 0;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-
-
-  $('.burger')[0].addEventListener('click', function(event) {
-    this.classList.toggle('active');
-    $('.dropDown')[0].classList.toggle('active');
-    $('header#header nav')[0].classList.toggle('active');
-    $('.blackscreen')[0].classList.toggle('active');
+  var nav = $('header#header nav')[0],
+      burger = $('.burger')[0],
+      dropDown = $('.dropDown')[0],
+      blackscreen = $('.blackscreen')[0],
+      dropDownLinks = $('a', dropDown),
+      products = Array.from($('.content .product > div, .content+.about > .close')).map(function(item) {
+        return {active: false,
+                type: item.className.indexOf('close')>=0?'close':'product',
+                element: item,
+                content: $('#'+item.getAttribute('name'))[0] || false,
+                changeState: function(type) {
+                  var e = this.element.classList, c = this.content.classList;
+                  if(type=='add') {
+                    e.add('active');
+                    c?c.add('active'):0;
+                  } else {
+                    e.remove('active');
+                    c?c.remove('active'):0;
+                  }
+                },
+               };
+      });
+  /* Sticky navbar scroll */
+  document.addEventListener('scroll', function() {
+    if(document.getScrollOffset()>=(document.getViewportHeight()-40)) {
+      nav.classList.add('dark');
+    } else {
+      nav.classList.remove('dark');
+    }
   });
+  /* Burger menu */
+  burger.addEventListener('click', function() {document.toggleClass('active', [burger, dropDown, blackscreen, nav])});
 
-  $('.blackscreen')[0].addEventListener('click', function(event) {
-    this.classList.toggle('active');
-    $('.dropDown')[0].classList.toggle('active');
-    $('header#header nav')[0].classList.toggle('active');
-    $('.burger')[0].classList.toggle('active');
-  });
+  /* blackscreen behind burger menu */
+  blackscreen.addEventListener('click', function() {burger.click()});
 
-  Array.from($('.dropDown ul a')).map(function(item) {
-    item.addEventListener('click', function(event) {
+  /* dropdown menu links */
+  Array.from(dropDownLinks).map(function(item) {
+    item.addEventListener('click', function() {
       item.classList.add('active');
       setTimeout(function() {
         item.classList.remove('active');
         window.location.hash = item.getAttribute('href');
-        $('.blackscreen')[0].classList.toggle('active');
-        $('.dropDown')[0].classList.toggle('active');
-        $('header#header nav')[0].classList.toggle('active');
-        $('.burger')[0].classList.toggle('active');
+        document.toggleClass('active', [blackscreen, dropDown, nav, burger]);
       }, 250);
-    });
-  })
-
-  Array.from($('.content .product > div, .content+.about > .close')).map(function(item, index, array) {
-    item.addEventListener('click', function(event) {
-      array.map(function(item2) {
-        item2.classList.remove('active');
-      })
-      item.classList.add('active');
-      Array.from($('.content+.about > section')).map(function(section) {
-        if(section.id === item.getAttribute('name')) {
-          section.classList.add('active');
-          if(section.getBoundingClientRect().top>Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
-            window.scroll(0,findPos(section));
-          }
-        } else {
-          section.classList.remove('active');
-        }
-      });
     });
   });
 
-  document.addEventListener('scroll', function(event) {
-    if((window.pageYOffset || document.documentElement.scrollTop)+25 >= Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) {
-      $('header#header nav')[0].classList.add('dark');
-    } else {
-      $('header#header nav')[0].classList.remove('dark');
-    }
+  /* products dropdown description */
+  products.map(function(item) {
+    item.element.addEventListener('click', function() {
+      for(var other of products) {
+        other.changeState('remove');
+        other.active = false;
+      }
+      item.active = true;
+      item.changeState('add');
+      if(item.content.getBoundingClientRect().top>document.getViewportHeight()) {
+        window.scroll(0, document.findPos(item.content)-50);
+      }
+    });
   });
 });
